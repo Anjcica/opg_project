003145108196f1a96b2fc3dbd8a2c5e8bb41484e
anjcica <anjcica.j@gmail.com>
2022-09-08 12:54:52 +0200

    staticfiles rollback

--- opg_project/settings.py
+++ opg_project/settings.py
@@ -150,12 +150,8 @@ FIXTURE_DIRS = os.path.join(BASE_DIR, 'opg/fixtures')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-
-STATIC_URL = os.getenv("STATIC_URL", "/static/")
-
-STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
