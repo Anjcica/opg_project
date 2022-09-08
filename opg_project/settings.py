5623b7611ba59749c99804120d281da0e0619107
anjcica <anjcica.j@gmail.com>
2022-09-08 10:03:06 +0200

    heroku database credentials

--- opg_project/settings.py
+++ opg_project/settings.py
@@ -85,18 +85,18 @@ WSGI_APPLICATION = 'opg_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'OPG',
-        'USER': 'admin',
-        'PASSWORD': 'Passw0rd',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'OPG',
+#         'USER': 'admin',
+#         'PASSWORD': 'Passw0rd',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 import dj_database_url
